14a24c0da6ff679bd0404d4add846837ac9eb02b
abbasgis@gmail.com <abbas123@abc>
2022-01-30 22:19:52 +0500

    Requirement file

--- spiu/wsgi.py
+++ spiu/wsgi.py
@@ -1,16 +1,26 @@
 """
-WSGI config for dgme_project project.
+WSGI config for ferrp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+sys.path.append('/home/env/python-www/spiu')
+sys.path.append('/home/env/python-www/spiu/venv/lib/python3.9/site-packages')
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
+from spiu import settings
+#
+# # add the virtualenv site-packages path to the sys.path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spiu.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spiu.settings")
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
